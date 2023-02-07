5261805094f84ca693f730da4fdbb5234e6d72d7
Felipe-Gs <felipegs2905@gmail.com>
2023-02-07 15:37:15 -0300

    feat: validacao de dados com Zod

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,3 +1,4 @@
+const Z = require("zod");
 const express = require('express');
 const router = express.Router();
 const { Client } = require('pg');
@@ -15,19 +16,26 @@ client.connect();
 //inserir
 router.post('/inserir', (req, res) => {
     try {
-        console.log(req.body);
-        const nome = req.body.nome;
-        const email = req.body.email;
-        const senha = req.body.senha;
+      const estudanteBody = Z.object({
+        email: Z.string().email(),
+        senha: Z.string(),
+        nome: Z.string().min(3)
+      }).required();
+      const validData = estudanteBody.parse(req.body);
+      const {email, senha, nome} = validData;
         const query = `
-          INSERT INTO estudante (nome, email, senha)
-          VALUES ('${nome}', '${email}', '${senha}');
+          INSERT INTO estudante (email, senha, nome)
+          VALUES ('${email}', '${senha}', '${nome}');
         `;
         client.query(query, (err, result) => {
           if (err) {
-            res.status(400).send('erro ao tentar iserir estudantes');
+            return res.status(400).send({
+              message: 'erro ao tentar cadastrar estudante'
+            });
           } else {
-            res.send(`estudante ${nome} inserido com sucesso`);
+            return res.send({
+              message: 'estudante inserido com sucesso'
+            });
           }
         });
         
@@ -45,7 +53,8 @@ router.get('/listar', (req, res) => {
             console.error(err.stack);
             res.status(500).send('Erro ao listar estudantes');
             } else {
-            res.send(result.rows);
+              console.table(result.rows)
+              res.send(result.rows);
             }
         });   
     } catch (error) {
@@ -70,6 +79,34 @@ router.delete('/deletar/:id', (req, res) => {
       }
     });
   });
+
+router.post('/login', (req, res) => {
+  try {
+    const estudanteBody = Z.object({
+      email: Z.string().email(),
+      senha: Z.string(),
+    }).required();
+
+    const validData = estudanteBody.parse(req.body);
+    const {email, senha} = validData;
+    console.log(email, senha)
+    const query = 'SELECT * FROM estudante WHERE email = $1 AND senha = $2';
+    client.query(query, [email, senha], (err, result) => {
+      if (err) {
+        console.error(err.stack);
+        res.status(500).send('Erro ao fazer login');
+      } else if (result.rows.length > 0) {
+        // Usuário encontrado, retornar alguma informação, como um token
+        res.send({ id, nome, email });
+      } else {
+        // Usuário não encontrado, retornar mensagem de erro
+        res.status(401).send({ success: false, message: 'Usuário ou senha inválidos' });
+      }
+    });
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 module.exports = router;
 
--- server/src/server.js
+++ server/src/server.js
@@ -7,6 +7,6 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use('/', routes);
 
-app.listen(3000, () => {
+app.listen(3000, '192.168.1.106', () => {
   console.log('Server running on http://localhost:3000');
 });
